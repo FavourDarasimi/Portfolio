4925875af7bf1416d75f16737fea0bea1f0db8cc
Darasimi <darasimi0603@gmail.com>
2025-10-14 16:22:56 +0100

    fix build

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -41,7 +41,7 @@ const Contact: React.FC<ContactProps> = ({ contactRef }) => {
       .then(() => {
         alert("Message sent successfully!");
       })
-      .catch((error) => {
+      .catch(() => {
         alert("Failed to send message");
       });
   };
